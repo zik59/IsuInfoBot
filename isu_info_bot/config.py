c249a6c15baef74b1b17de4f8f347e58021a0214
zik59 <mihael-keel@mail.ru>
2023-04-02 01:08:56 +0300

    wrapped with docker

--- isu_info_bot/config.py
+++ isu_info_bot/config.py
@@ -14,7 +14,7 @@ BASE_DIR = Path(__file__).resolve().parent
 SQLITE_DB_FILE = BASE_DIR / "db.sqlite3"
 TEMPLATES_DIR = BASE_DIR / "templates"
 
-PAGE_SIZE = 20
+PAGE_SIZE = 10
 
 VARIANT_CALLBACK_PATTERN = "variant_"
 GROUP_CALLBACK_PATTERN = "group_"
